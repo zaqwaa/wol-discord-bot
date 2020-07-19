c691685ff986603aac4cdcdbcc7f0314f0f03fce
Ubuntu <k.jeremy.l@gmail.com>
2020-07-19 17:38:29 +0000

    add IsPortOpen for cleanup

--- bot.py
+++ bot.py
@@ -35,6 +35,11 @@ bot = commands.Bot(command_prefix='$', description = description)
 # ports to look for servers
 server_port_list = [int(e) for e in MC_SERVER_PORTS.split(",")]
 
+def IsPortOpen(ip, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.settimeout(1)
+        return not bool(sock.connect_ex((ip, int(port))))       # returns 0 for success
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0}#{1}'.format(bot.user.name,bot.user.discriminator))
@@ -54,62 +59,52 @@ async def startmcserver(ctx):
 @bot.command()
 async def checkmcserver(ctx):
     """Check if the MC control panel interface port is open"""
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        sock.settimeout(1)
-        if sock.connect_ex((MC_SERVER_IP, MC_SERVER_INTERFACE_PORT)) == 0:
-            await ctx.send("Interface port is open! Server is most likely up.")
-        else:
-            await ctx.send("Interface port is not open... server may be down")
+    if IsPortOpen(MC_SERVER_IP, MC_SERVER_INTERFACE_PORT):
+        await ctx.send("Interface port is open! Server is most likely up.")
+    else:
+        await ctx.send("Interface port is not open... server may be down")
 
 @bot.command()
 async def mcserverstats(ctx):
     """Check if the MC control panel interface port is open"""
     servers_online = 0
     output_message = ""
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        sock.settimeout(1)
-        if sock.connect_ex((MC_SERVER_IP, MC_SERVER_INTERFACE_PORT)) == 0:
-            for port in server_port_list:
-                with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as gameservsock:
-                    gameservsock.settimeout(1)
-                    if gameservsock.connect_ex((MC_SERVER_IP, port)) == 0:
-                        servers_online = servers_online + 1
-                        mcserver = MinecraftServer(MC_SERVER_IP, port)
-                        status = mcserver.status()
-                        player_list = [player.name for player in status.players.sample] if status.players.sample is not None else ["No players connected"]
-                        players = ", ".join(player_list)
-                        server_info = "*{name}*@{ip}:{port}: {version} \n\t{players}".format(
-                                name = status.description['text'], 
-                                ip = MC_SERVER_IP,
-                                port = port,
-                                version = status.version.name, 
-                                players = players)
-                        output_message = output_message + server_info + "\n"
-            if(servers_online > 0):
-                await ctx.send(output_message)
-            else:
-                await ctx.send("No servers online.")
+    if IsPortOpen(MC_SERVER_IP, MC_SERVER_INTERFACE_PORT):
+        for port in server_port_list:
+            if IsPortOpen(MC_SERVER_IP, port):
+                servers_online = servers_online + 1
+                mcserver = MinecraftServer(MC_SERVER_IP, port)
+                status = mcserver.status()
+                player_list = [player.name for player in status.players.sample] if status.players.sample is not None else ["No players connected"]
+                players = ", ".join(player_list)
+                server_info = "*{name}*@{ip}:{port}: {version} \n\t{players}".format(
+                        name = status.description['text'],
+                        ip = MC_SERVER_IP,
+                        port = port,
+                        version = status.version.name,
+                        players = players)
+                output_message = output_message + server_info + "\n"
+        if(servers_online > 0):
+            await ctx.send(output_message)
         else:
-            await ctx.send("Interface port is not open... no servers to check")
+            await ctx.send("No servers online.")
+    else:
+        await ctx.send("Interface port is not open... no servers to check")
 
 @tasks.loop(seconds=15)
 async def set_bot_presence():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        sock.settimeout(1)
-        if sock.connect_ex((MC_SERVER_IP, MC_SERVER_INTERFACE_PORT)) == 0:
-            players_online = 0
-            for port in server_port_list:
-                with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as gameservsock:
-                    gameservsock.settimeout(1)
-                    if gameservsock.connect_ex((MC_SERVER_IP, port)) == 0:
-                        mcserver = MinecraftServer(MC_SERVER_IP, port)
-                        status = mcserver.status()
-                        players_online = players_online + status.players.online
-            game = discord.Game("Server is up! {0} player{1} online".format(players_online, "s" if players_online != 1 else ""))
-            await bot.change_presence(status=discord.Status.online,activity=game)
-        else:
-            game = discord.Game("Server is down.")
-            await bot.change_presence(status=discord.Status.idle,activity=game)
+    if IsPortOpen(MC_SERVER_IP, MC_SERVER_INTERFACE_PORT):
+        players_online = 0
+        for port in server_port_list:
+            if IsPortOpen(MC_SERVER_IP, port):
+                mcserver = MinecraftServer(MC_SERVER_IP, port)
+                status = mcserver.status()
+                players_online = players_online + status.players.online
+        game = discord.Game("Server is up! {0} player{1} online".format(players_online, "s" if players_online != 1 else ""))
+        await bot.change_presence(status=discord.Status.online,activity=game)
+    else:
+        game = discord.Game("Server is down.")
+        await bot.change_presence(status=discord.Status.idle,activity=game)
 
 @set_bot_presence.before_loop
 async def before_set_bot_presence():
